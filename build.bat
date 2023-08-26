b060e1214bcf77a5a37960f22142cfaf95a38c9d
Violet Bit Kitten <>
2023-08-26 19:02:14 -0400

    Moved all source files into a subdirectory.

--- build.bat
+++ build.bat
@@ -3,6 +3,10 @@ REM Batch file for building VTuberVoice on Windows.
 REM See the following for help in BAT scripts.
 REM https://en.wikibooks.org/wiki/Windows_Batch_Scripting
 
+REM Variables for the build.
+SET SOURCEDIR="source\"
+SET APPNAME="VTuberVoice"
+
 REM Check for a command line argument.
 REM If none just run the build.
 IF -%1-==-- GOTO runbuild
@@ -23,38 +27,44 @@ REM Figure out what we were given on the command line.
 
 REM Clean and Build.
 :runall
-    CALL :runclean
+    IF EXIST "%SOURCEDIR%Makefile" (
+        CALL :runclean
+    )
     CALL :runbuild
     EXIT /b
 
 REM Run the actual build.
 :runbuild
-    ECHO Building VTuberVoice...
+    ECHO Building %APPNAME%...
+    CD %SOURCEDIR%
     IF NOT EXIST "Makefile" (
         fpcmake
     )
     make
+    cd ..
     EXIT /b
 
 REM Cleanup build artifacts, including the Makefile.
 :runclean
-    ECHO Cleaning VTuberVoice...
+    ECHO Cleaning %APPNAME%...
+    CD %SOURCEDIR%
     fpcmake
     make distclean
     DEL Makefile
+    cd ..
     EXIT /b
 
 REM Show some help text.
 :showhelp
-    ECHO VTuberVoice Windows Build Script
-    ECHO Performs build tasks for VTuberVoice.
+    ECHO %APPNAME% Windows Build Script
+    ECHO Performs build tasks for %APPNAME%.
     ECHO.
     ECHO Commands:
     ECHO all   - Build everything from scratch. (Clean then Build)
-    ECHO build - Builds the VTuberVoice project. (Default)
+    ECHO build - Builds the %APPNAME% project. (Default)
     ECHO clean - Runs the make command 'distclean'.
     ECHO help  - Display this help text.
-    ECHO test  - Runs the VTuberVoice tests.
+    ECHO test  - Runs the %APPNAME% tests.
     GOTO exitscript
 
 REM Run the tests.
@@ -62,7 +72,7 @@ REM Run the tests.
     IF NOT EXIST "rununit.exe" (
         CALL :runbuild
     )
-    ECHO Running VTuberVoice Unit tests...
+    ECHO Running %APPNAME% Unit tests...
     rununit
     EXIT /b
 
