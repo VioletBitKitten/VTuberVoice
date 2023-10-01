2d046520eb612ff1e6cb1baf0a6dce995aa02386
Violet Bit Kitten <140219752+VioletBitKitten@users.noreply.github.com>
2023-09-30 20:40:06 -0400

    Minor improvements.

--- testvtv.bat
+++ testvtv.bat
@@ -2,22 +2,25 @@
 REM Run some tests of the VTuberVoice application.
 REM I keep missing bugs because I don't fully test the program.
 
+REM ----------========== Setup ==========----------
+
 REM Variables for testing.
 SET APPNAME="VTuberVoice"
 SET OUTPUTFILE=output.txt
+SET ERRORCOUNT=0
 
 ECHO Running tests of %APPNAME%.
-ECHO Testing will abort on the first failure.
 ECHO Writing output to the file: %OUTPUTFILE%
 ECHO.
 
 REM Make sure the application has been built.
 IF NOT EXIST "vtv.exe" (
     ECHO Building the %APPNAME% application.
-    CALL build > NUL
+    CALL build > %OUTPUTFILE% 2>&1
     IF %ERRORLEVEL% neq 0 (
-        ECHO The build failed. Please run the build again and check for errors.
-        GOTO exitscript
+        ECHO The build failed.
+        ECHO See the file %OUTPUTFILE% for errors.
+        GOTO :EOF
     )
     ECHO.
 )
@@ -26,6 +29,8 @@ REM If the output file exists delete it.
 IF EXIST "%OUTPUTFILE%" (
     DEL "%OUTPUTFILE%"
 )
+
+REM ----------========== Run the tests ==========----------
 
 CALL :RunTest ^
     "Generating help text to check %APPNAME% starts successfully..." ^
@@ -67,35 +72,51 @@ CALL :RunTest ^
     "Speaking text in interactive mode..." ^
     "vtv.exe < testfiles/interactivespeech.txt >> %OUTPUTFILE% 2>&1"
 
-REM Finished with tests.
-CALL :OutputString "Testing successful."
-GOTO :exitscript
+REM Finished with tests. Check if there were any failures.
+CALL :OutputString
+if %ERRORCOUNT% NEQ 0 (
+    CALL :OutputString "%ERRORCOUNT% tests have failed."
+    CALL :OutputString "See the file %OUTPUTFILE% for possible errors."
+) else (
+    CALL :OutputString "Testing successful."
+)
+GOTO :EOF
+
+REM ----------========== Subroutines ==========----------
 
 REM Run a test.
 REM %1 = The string to print.
 REM %2 = The test to run.
 :RunTest
-    CALL :OutputString "%~1%"
+    CALL :OutputStringNONL "%~1%"
     %~2
-    if %ERRORLEVEL% equ 0 (
+    if %ERRORLEVEL% EQU 0 (
         CALL :OutputString "SUCCEEEDED"
     ) else (
         CALL :OutputString "FAILED"
-        GOTO exitscript
+        SET /A ERRORCOUNT = %ERRORCOUNT% + 1
     )
-    CALL :OutputString
     EXIT /b
 
 REM Write a string to STDOUT and the log file.
 :OutputString
     IF -%1-==-- (
         ECHO.
-        ECHO. >> %OUTPUTFILE%
+        ECHO.>> %OUTPUTFILE%
     ) else (
         ECHO %~1
-        ECHO %~1 >> %OUTPUTFILE%
+        ECHO %~1>> %OUTPUTFILE%
     )
     EXIT /b
 
-REM Exit the script.
-:exitscript
+REM Write a string to STDOUT and the log file. Special case.
+REM No newline is output to STDOUT and a space is added to the end.
+:OutputStringNONL
+    IF -%1-==-- (
+        ECHO.
+        ECHO.>> %OUTPUTFILE%
+    ) else (
+        set <NUL /p="%~1 "
+        ECHO %~1>> %OUTPUTFILE%
+    )
+    EXIT /b
